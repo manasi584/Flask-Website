39888e0685bc53d8ecf3dd19a021774a82555498
manasi584 <120204669+manasi584@users.noreply.github.com>
2024-04-18 17:43:07 +0000

    Added dynamic data and API route

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-from flask import Flask
+from flask import Flask,render_template,jsonify
 
 app = Flask(__name__)
 
+COURSE=[
+  {
+    'id':1,
+    'title':'Data Analysis',
+    'location':'Online',
+  },
+  {
+    'id':2,
+    'title':'Fundamentals of Options Trading',
+    'location':'Offline'
+    
+  },
+  {
+    'id':3,
+    'title':'Indian Economy -A Case Study',
+    'location':'Hybrid'
+  }
+]
+
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('home.html',course=COURSE,company="InvestLearn")
+
+#creating a json end point instead of html
+@app.route("/courses")
+def courses():
+  return jsonify(COURSE)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
